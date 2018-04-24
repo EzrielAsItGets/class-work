3b46d3920fe6ca81e45bfa3fea0785eab93001c8
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2018-04-24 12:03:36 -0400

    Update

--- cisc3300/final_project/q2/first.js
+++ cisc3300/final_project/q2/first.js
@@ -1,1 +1,17 @@
-# placeholder
+function searchFunction() {
+  var term = $('#searchtext').val().trim().toLowerCase();
+  if (term.length > 0) {
+    var source = $('.main').text();
+    var words = source.split(' ');
+    var output = '';
+    $.each(words, function(idx, word) {
+      if (term === word.toLowerCase()) {
+        output += '<span class="highlight">' + word + '</span> ';
+      }
+      else {
+        output += word + ' ';
+      }
+    });
+    $('.main').html(output);
+  }
+}
