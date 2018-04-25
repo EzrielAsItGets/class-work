1c8a2868b1f83f3cfa61a54d3927e1463cc0f3ba
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2018-04-25 02:09:02 -0400

    Update

--- cisc3300/final_project/q2/first.js
+++ cisc3300/final_project/q2/first.js
@@ -1,8 +1,11 @@
 functionSearch() {
-  var searchPhrase = document.getElementById('searchtext');
-  var mainParas = document.querySelectorAll('#main');
+  var searchPhrase = document.getElementById("searchtext");
+  var mainParas = document.querySelectorAll("#main");
+  var str = "";
   for (var i = 0; i < mainParas.length; i++) {
-    mainParas[i].textContent.indexOf(searchPhrase) >= 0);
+    if (mainParas[i].textContent.indexOf(searchPhrase) >= 0))
+      text += "<span class = 'highlight'>" + mainParas + "</span>";
+    else
+      text += mainParas;
   }
-  $('#main:contains(searchPhrase)').setAttribute('class', 'highlight');
 }
