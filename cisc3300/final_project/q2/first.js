b694598ad2a0286acb60ae034d0f0e49a2317121
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2018-04-25 15:05:41 -0400

    Update

--- cisc3300/final_project/q2/first.js
+++ cisc3300/final_project/q2/first.js
@@ -1,11 +1,10 @@
-functionSearch() {
-  var searchPhrase = document.getElementById("searchtext");
-  var mainParas = document.querySelectorAll("#main");
-  var str = "";
-  for (var i = 0; i < mainParas.length; i++) {
-    if (mainParas[i].textContent.indexOf(searchPhrase) >= 0))
-      text += "<span class = 'highlight'>" + mainParas + "</span>";
-    else
-      text += mainParas;
-  }
+function highlightSearch() {
+    var keyword = document.getElementById("searchtext").value;
+    var highlight = new RegExp("(\\b" + keyword + "\\b)", "gim");
+    var paragraph = document.getElementById("main").innerHTML;
+    var paragraphnew = paragraph.replace(/(<span>|<\/span>)/igm, "");
+    document.getElementById("main").innerHTML = paragraphnew;
+    var newparagraph = paragraphnew.replace(highlight, "<span>$1</span>");
+    document.getElementById("main").innerHTML = newparagraph;
+
 }
