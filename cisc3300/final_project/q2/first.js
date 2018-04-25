adc3792e4dc3df43cba80d3c2c8bf25d817436c4
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2018-04-25 00:27:05 -0400

    Update

--- cisc3300/final_project/q2/first.js
+++
@@ -1,5 +0,0 @@
-function searchFunction() {
-  var searchPhrase = document.getElementById("searchtext");
-  mainParas[i].textContent.indexOf(searchPhrase) >= 0);
-  var searchtext = document.querySelectorAll("searchPhrase");
-}
