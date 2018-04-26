f7d87bb1b30fb53c475bffcb02f44af9c1f07f2c
luciavacc <38732534+luciavacc@users.noreply.github.com>
2018-04-26 00:39:45 -0400

    Update innerconflict.js

--- cisc3300/final_project/q4/innerconflict.js
+++ cisc3300/final_project/q4/innerconflict.js
@@ -1,16 +1,16 @@
 function onClick() {
   for(i = 1; i < 13; i++){
 	  div1 = document.createElement('div');
-	  div1.style.top = 500 + (i-1);
-	  div1.style.left = 500 + (i-1);
-	  div1.style.width = 400 - 2*(i-1) * delta;
-	  div1.style.height = 400 - (i-1 ) * delta;
+	  div1.style.left = 50 + (i*10);
+	  div1.style.top = 50 + (i*10);
+	  div1.style.width = 400 - (i*10);
+	  div1.style.height = 400 -(i*10);
 	  body.appendChild(div1);
-	  if(i >1)
-		  div1.style.backgroundColor = (100,0,0);
+	  if(i%2 > 0)
+		  div1.style.backgroundColor = (34,12,64);
 	  else
-		  div1.style.backgroundColor = (0,0,100);
+		  div1.style.backgroundColor = (76,0,34);
   }
-	
+}	
 button1 = document.querySelector('button');
 	button1.addEventListener('click', onClick);
