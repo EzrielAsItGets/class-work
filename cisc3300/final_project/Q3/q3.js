86bbe4f30c42c90220ee19ae6acce4b95c9886b6
Priska <33299111+pmohunsingh@users.noreply.github.com>
2018-04-26 00:35:34 -0400

    Create q3.js

---
+++ cisc3300/final_project/Q3/q3.js
@@ -0,0 +1,15 @@
+function first(){
+ var First = document.getElementById("First");
+ First.innerHTML= "<img src= 'City001.jpg' alt='Image' style='width:14em; height:14em;' />";
+ 
+}
+function second(){
+ document.getElementById("Second").innerHTML= "<img src='City001.jpg' alt='Image' style='width:14em; height:14em;' />";
+ 
+ 
+}
+function third(){
+ document.getElementById("Third").innerHTML= "<img src= 'City001.jpg' alt='Image' style='width:14em; height:14em;' />";
+ 
+ 
+}
