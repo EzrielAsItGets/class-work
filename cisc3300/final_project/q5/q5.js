94bc2b0d62b0e9f7f3e286b3c6d48aea3ade2cf6
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2018-05-01 10:00:50 -0400

    Updated q5

--- cisc3300/final_project/q5/q5.js
+++ cisc3300/final_project/q5/q5.js
@@ -1,15 +1,37 @@
-function first(){
- var First = document.getElementById("First");
- First.innerHTML= "<img src= 'City001.jpg' alt='Image' style='width:14em; height:14em;' />";
- 
-}
-function second(){
- document.getElementById("Second").innerHTML= "<img src='City001.jpg' alt='Image' style='width:14em; height:14em;' />";
- 
- 
-}
-function third(){
- document.getElementById("Third").innerHTML= "<img src= 'City001.jpg' alt='Image' style='width:14em; height:14em;' />";
- 
- 
+class button {
+
+  constructor(container){
+    this.container = document.querySelectorAll(container)[0];
+    console.log(this.container)
+    var items = this.container.children;
+
+    var fnArr = [this.first, this.second, this.third]
+    var ctr = 0;
+    for(var child in items){
+
+        if(items[parseInt(child)]){
+          items[parseInt(child)].addEventListener('click', fnArr[ctr])
+        }
+
+        ctr += 1
+    }
+
+  }
+
+
+  first(elem){
+    var elem = this;
+     elem.innerHTML= "<img src= 'gorilla.bmp' alt='Image' style='width:14em; height:14em;' />";
+  }
+
+  second(elem){
+    var elem = this;
+     elem.innerHTML= "<img src='City002.jpg' alt='Image' style='width:14em; height:14em;' />";
+
+  }
+
+  third(elem){
+    var elem = this;
+     elem.innerHTML= "<img src= 'City001.jpg' alt='Image' style='width:14em; height:14em;' />";
+  }
 }
