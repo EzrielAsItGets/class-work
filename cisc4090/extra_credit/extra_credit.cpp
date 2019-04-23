c102e689306fea32642b18801c5fd941b87be106
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2019-04-22 23:41:00 -0400

    Create extra_credit.cpp

---
+++ cisc4090/extra_credit/extra_credit.cpp
@@ -0,0 +1,78 @@
+/*
+ * Ezriel Ciriaco
+ * CISC 4090 L01
+ * Due: 25 April 2019
+ * EC2: Program that outputs the pairs of integers contained in the vector that add up to the specified value of SUM.
+*/
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include <limits>
+
+const int SIZE = 8; // Prevent users from entering an absurd value for DIST_INT for vector initialization.
+
+using namespace std;
+
+void user_input(int &a, int &b);
+bool valid_input(int a);
+void initialize_vector(vector<int> &v, int a);
+void print_vector(vector<int> v, int a);
+
+int main() {
+  srand(time(NULL));
+  vector<int> vector;
+  int DIST_INT = 0;
+  int SUM = 0;
+
+  do {
+    user_input(DIST_INT, SUM);
+    if (!valid_input(DIST_INT))
+      cout << "\nThese values are invalid!\n";
+  } while(!valid_input(DIST_INT));
+
+  initialize_vector(vector, DIST_INT);
+  print_vector(vector, DIST_INT);
+
+  return 0;
+}
+
+void user_input(int &a, int &b) {
+  cout << "\nNumber of Distinct Integers (Must be <= 8): ";
+  cin >> a;
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout << "\nPlease enter an INTEGER for Distinct Integers: ";
+    cin >> a;
+  }
+  cout << "\nValue for SUM: ";
+  cin >> b;
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout << "\nPlease enter an INTEGER for SUM: ";
+    cin >> b;
+  }
+}
+
+bool valid_input(int a) {
+  if (a <= SIZE)
+    return true;
+  else
+    return false;
+}
+
+void initialize_vector(vector<int> &v, int a) {
+  for (int i = 0; i < a; i++)
+    v.push_back((double) 9 * rand() / RAND_MAX);
+}
+
+void print_vector(vector<int> v, int a) {
+  cout << "\nVector: ";
+  for (int i = 0; i < a; i++)
+    cout << setw(2) << v[i];
+  cout << endl;
+}
